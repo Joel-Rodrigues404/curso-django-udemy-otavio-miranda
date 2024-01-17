283bc3cf73d3f47fa0369550fe56d6aae315136e
Joel Rodrigues <joelsandersonrodrigues@gmail.com>
2024-01-16 21:05:29 -0300

    Configurando arquivos estaticos

--- estudo.md
+++ estudo.md
@@ -1,3 +1,4 @@
 
 ### não e uma boa pratica deixar nomeclaturas do projeto em diferentes linguagesn e bom sempre deixar em ingles
 ### none e o retorno padrão quando a função não retorna nada
+### arquivos estaticos do django são armazenados no cache do navegador por padrão
--- projeto/settings.py
+++ projeto/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #Ddjango apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Project apps
     'recipes.apps.RecipesConfig',
 ]
 
