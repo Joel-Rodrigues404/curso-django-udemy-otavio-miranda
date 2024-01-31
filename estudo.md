67e04c5044fbdd0a442d75113c112687c72b5410
Joel Rodrigues <joelsandersonrodrigues@gmail.com>
2024-01-31 18:26:03 -0300

    Estilizando pagina de recipe

--- estudo.md
+++ estudo.md
@@ -2,3 +2,4 @@
 ### não e uma boa pratica deixar nomeclaturas do projeto em diferentes linguagesn e bom sempre deixar em ingles
 ### none e o retorno padrão quando a função não retorna nada
 ### arquivos estaticos do django são armazenados no cache do navegador por padrão
+### Load static carrega por arquivo
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes import factory
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    context = {
+        'recipes':[factory.make_recipe() for _ in range(10)]
+    }
+
+    return render(request, 'recipes/pages/home.html', context=context)
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+
+    context = {
+        'recipe': factory.make_recipe(),
+        'is_detail_page':True,
+    }
+    return render(request, 'recipes/pages/recipe-view.html', context=context)
     
